4df430e757ea30af72cee47a902e0d26e56b378f
Sorel-DSK <sorel7005@gmail.com>
2025-05-06 01:50:08 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const btnAjouter=document.getElementById('btnAjouter')
+
+    btnAjouter.addEventListener('click',()=>{
+         //recuperer les elements
+        const tache=document.getElementById('todo-input').value.trim()
+        const list=document.getElementById('list')
+        //verifier si le champ est vide ou pas
+        if (!tache) {
+          return;
+        }else{
+           
+            // cree la tache et l'ajouter a la liste
+            const li=document.createElement('li')
+            li.textContent=tache
+            list.appendChild(li)
+              //reinstialiser l'input
+            tache.value=" "
+            //cree un btn supprimer
+            const delBtn=document.createElement('button')
+            delBtn.textContent="Supprimer"
+            delBtn.classList.add('.delbtnstyle')
+            delBtn.addEventListener('click',()=>{
+                list.removeChild(li)
+            })
+            li.appendChild(delBtn)
+        }
+    })
+
+   
+// hostinger
+// netlifly
+// vercel
+// github page
